4e904975102c037dec38a97d46b2d71a4fbe491d
ripee <ripee.dev@gmail.com>
2019-11-27 22:53:07 +0100

    noblelte-common: Move libnfc* and nfcee_access.xml to common vendor

--- noblelte-common.mk
+++ noblelte-common.mk
@@ -248,12 +248,6 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
     android.hardware.nfc@1.0-service
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
-    $(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/vendor/etc/libnfc-sec-hal.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/vendor/etc/libnfc-sec.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/vendor/etc/libnfc-nci.conf
-
 # Network
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
